28785cd4bdd13cd766f6373e9b1e58ae86c91c29
Faqihuddin Al-Ghiffary <148179530+f4qihuddin@users.noreply.github.com>
2025-10-25 17:27:18 +0700

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -6,56 +6,66 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 BASE = Path(__file__).resolve().parents[1]
 
 # Gejala dikategorikan
-GEJALA = {
-    "Kerusakan pada Daun": {
-        "A1": "Ujung atau tepi daun berwarna cokelat keabu-abuan",
-        "A2": "Daun memiliki bercak cokelat berbentuk belah ketupat",
-        "A3": "Daun memiliki bercak cokelat sempit dan memanjang",
-        "A4": "Terdapat bercak putih pada daun berupa garis atau gulungan",
-        "A5": "Daun berwarna kuning hingga oranye kekuningan, terlihat wereng hijau pada daun",
-        "B1": "Daun menguning lalu mengering",
-        "B2": "Ujung atau tepi daun berwarna cokelat keabu-abuan dan kemudian menyebar ke seluruh daun",
-        "B3": "Ujung dan tepi daun mengering serta layu"
-    },
-    "Kerusakan pada Batang": {
-        "A6": "Batang mengering dan bagian pangkal batang tampak cokelat akibat wereng cokelat",
-        "A7": "Pucuk batang padi mengering dan mudah dicabut",
-        "A8": "Di dalam batang tanaman terdapat larva atau ulat",
-        "A9": "Batang tanaman berwarna kuning atau cokelat kemerahan dan terdapat bercak hitam",
-        "A10": "Batang padi roboh, terlihat adanya serangan tikus",
-        "B4": "Malai padi busuk atau patah pada fase generatif",
-        "B5": "Batang dan tulang daun membusuk sehingga tanaman rebah",
-        "B6": "Tangkai malai padi patah dan rusak"
-    },
-    "Kerusakan pada Gabah": {
-        "A11": "Terdapat bercak hitam pada gabah",
-        "A12": "Gabah rontok, terlihat burung di area sawah",
-        "B7": "Jumlah malai sedikit dan gabah banyak yang kosong",
-        "B8": "Gabah tidak terisi penuh atau hampa",
-        "B9": "Malai padi berdiri tegak namun gabah tidak terisi penuh atau hampa",
-        "B10": "Warna gabah berubah dan rasanya menjadi tidak enak"
-    },
-    "Kerusakan akibat Faktor Luar": {
-        "B11": "Tanaman layu dan mati pada awal pertumbuhan",
-        "B12": "Daun tanaman berwarna kuning oranye",
-        "B13": "Pertumbuhan tanaman kerdil",
-        "B14": "Pertumbuhan tanaman terhambat",
-        "B16": "Tanaman muda mati dan mengering",
-        "B17": "Penggunaan pupuk nitrogen (N) secara berlebihan",
-        "B20": "Terdapat jejak kaki dan lubang tikus di sekitar sawah"
-    }
+gejala_dict = {
+    "G01": "Nafsu makan menurun",
+    "G02": "Sesak napas / megap-megap",
+    "G03": "Napas mendengkur basah",
+    "G04": "Bersin",
+    "G05": "Batuk",
+    "G06": "Bulu kusam dan keriput",
+    "G07": "Diare",
+    "G08": "Produksi telur menurun",
+    "G09": "Membeku",
+    "G10": "Terlihat lesu",
+    "G11": "Diare kehijauan",
+    "G12": "Diare keputihan",
+    "G13": "Wajah pucat",
+    "G14": "Terlihat kebiruan",
+    "G15": "Bercak membengkak",
+    "G16": "Jengger pucat",
+    "G17": "Kaki dan sayap lumpuh",
+    "G18": "Keluar cairan dari mata dan hidung",
+    "G19": "Kepala bengkak",
+    "G20": "Kepala berputar",
+    "G21": "Pembengkakan sinus dan mata",
+    "G22": "Perut membesar",
+    "G23": "Sayap menggantung",
+    "G24": "Terdapat kotoran putih di sekitar anus",
+    "G25": "Kematian mendadak",
+    "G26": "Kulit telur kasar",
+    "G27": "Putih telur encer",
+    "G28": "Feses kuning kehijauan",
+    "G29": "Pembengkakan area wajah dan sekitar mata",
+    "G30": "Feses berdarah",
+    "G31": "Berkelompok di sudut kandang",
+    "G32": "Mematuk area kloaka",
+    "G33": "Mematuk area kloaka",
+    "G34": "Telur lebih kecil",
+    "G35": "Lumpuh pada tembolok",
+    "G36": "Bernapas lewat mulut sambil menjulurkan leher",
+    "G37": "Batuk berdarah",
+    "G38": "Tidur dengan paruh menyentuh lantai",
+    "G39": "Duduk dengan posisi membungkuk",
+    "G40": "Terlihat mengantuk dengan bulu berdiri",
+    "G41": "Tubuh kurus",
+    "G42": "Ada lendir bercampur darah di rongga mulut",
+    "G43": "Kaki pincang"
 }
 
-PENYAKIT = {
-    "P01": "Hawar Daun Bakteri",
-    "P02": "Penyakit BLAS",
-    "P03": "Bercak Daun Cokelat Sempit",
-    "P04": "Wereng Hijau & Tungro",
-    "P05": "Wereng Cokelat",
-    "P06": "Penggerek Batang Padi",
-    "P07": "Serangga Padi Hitam",
-    "P08": "Tikus Sawah",
-    "P09": "Serangga Padi Berbau"
+penyakit_dict = {
+    "P01": "Tinja berwarna kapur (Pullorum)",
+    "P02": "Kolera ayam (Fowl Cholera)",
+    "P03": "Flu burung (Avian Influenza)",
+    "P04": "Totelo / ND (Newcastle Disease)",
+    "P05": "Ayam tifus (Fowl Typhoid)",
+    "P06": "Diare (Coccidiosis)",
+    "P07": "Gumboro (Gumboro Disease)",
+    "P08": "Ayam salesma (Infectious Coryza)",
+    "P09": "Batuk ayam kronis (Infectious Bronchitis)",
+    "P10": "Busung ayam (Lymphoid Leukosis)",
+    "P11": "Batuk berdarah (Infectious Laryngotracheitis)",
+    "P12": "Mareks (Marek's Disease)",
+    "P13": "Penurunan produksi telur (Egg Drop Syndrome 76 / EDS 76)"
 }
 
 
@@ -64,17 +74,17 @@ RULES_PATH = RULES_PATH.resolve()
 
 @app.route("/")
 def index():
-    return render_template("index.html", gejala=GEJALA)
+    return render_template("index.html", gejala=gejala_dict)
 
 @app.route("/diagnose", methods=["POST"])
 def diagnose():
     data = request.json
     selected = data.get("gejala", [])
-    fact_cfs = data.get("fact_cfs", {})
+    fact_cfs = data.get("user_cfs", {})
     rules = load_rules(RULES_PATH)
     results = forward_infer(selected, fact_cfs, rules)
     for r in results:
-        r["label"] = PENYAKIT.get(r["conclusion"], r["conclusion"])
+        r["label"] = penyakit_dict.get(r["conclusion"], r["conclusion"])
     return jsonify({"results": results})
 
 if __name__ == "__main__":
